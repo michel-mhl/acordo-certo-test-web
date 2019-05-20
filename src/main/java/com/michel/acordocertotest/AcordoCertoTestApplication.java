ae485310035a0005d274efcf7d1aef610f437ae9
michel <michel_mhl@hotmail.com>
2019-05-20 17:22:20 -0300

    projeto criado

---
+++ src/main/java/com/michel/acordocertotest/AcordoCertoTestApplication.java
@@ -0,0 +1,13 @@
+package com.michel.acordocertotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcordoCertoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcordoCertoTestApplication.class, args);
+	}
+
+}
